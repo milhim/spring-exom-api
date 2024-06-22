c72594df2d3d99c88e57186bde38c8e398836913
Milhim Milhim <milhim.a.milhim@gmail.com>
2024-06-22 22:32:16 +0300

    init

---
+++ src/main/java/com/milhim/ecommerce/ecommerce/EcommerceApplication.java
@@ -0,0 +1,13 @@
+package com.milhim.ecommerce.ecommerce;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EcommerceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EcommerceApplication.class, args);
+	}
+
+}
