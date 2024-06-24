45eee92d53521d76e12a3af5516a91cf7f902e66
Milhim Milhim <milhim.a.milhim@gmail.com>
2024-06-24 22:01:20 +0300

    implement login

--- src/main/java/com/milhim/ecommerce/ecommerce/api/controller/auth/AuthenticationController.java
+++ src/main/java/com/milhim/ecommerce/ecommerce/api/controller/auth/AuthenticationController.java
@@ -1,5 +1,7 @@
 package com.milhim.ecommerce.ecommerce.api.controller.auth;
 
+import com.milhim.ecommerce.ecommerce.api.model.LoginBody;
+import com.milhim.ecommerce.ecommerce.api.model.LoginResponse;
 import com.milhim.ecommerce.ecommerce.api.model.RegistrationBody;
 import com.milhim.ecommerce.ecommerce.exception.UserAlreadyExistException;
 import com.milhim.ecommerce.ecommerce.service.UserService;
@@ -31,5 +33,18 @@ public class AuthenticationController {
         } catch (UserAlreadyExistException ex) {
             return ResponseEntity.status(HttpStatus.CONFLICT).build();
         }
+    }
+
+    @PostMapping("/login")
+    public ResponseEntity<LoginResponse> loginUser(@Valid @RequestBody LoginBody loginBody) {
+        String jwt = this.userService.loginUser(loginBody);
+        if (jwt == null) {
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).build();
+        }
+
+        LoginResponse response = new LoginResponse();
+        response.setJwt(jwt);
+        return ResponseEntity.ok(response);
+
     }
 }
--- src/main/java/com/milhim/ecommerce/ecommerce/service/UserService.java
+++ src/main/java/com/milhim/ecommerce/ecommerce/service/UserService.java
@@ -1,20 +1,25 @@
 package com.milhim.ecommerce.ecommerce.service;
 
+import com.milhim.ecommerce.ecommerce.api.model.LoginBody;
 import com.milhim.ecommerce.ecommerce.api.model.RegistrationBody;
 import com.milhim.ecommerce.ecommerce.exception.UserAlreadyExistException;
 import com.milhim.ecommerce.ecommerce.model.LocalUser;
 import com.milhim.ecommerce.ecommerce.model.repository.LocalUserRepository;
 import org.springframework.stereotype.Service;
 
+import java.util.Optional;
+
 @Service
 public class UserService {
 
     private final LocalUserRepository localUserRepository;
     private final EncryptionService encryptionService;
+    private final JWTService jwtService;
 
-    public UserService(LocalUserRepository localUserRepository, EncryptionService encryptionService) {
+    public UserService(LocalUserRepository localUserRepository, EncryptionService encryptionService, JWTService jwtService) {
         this.localUserRepository = localUserRepository;
         this.encryptionService = encryptionService;
+        this.jwtService = jwtService;
     }
 
     public LocalUser registerUser(RegistrationBody registrationBody) throws UserAlreadyExistException {
@@ -32,5 +37,18 @@ public class UserService {
         localUser.setPassword(encryptionService.encryptPassword(registrationBody.getPassword()));
 
         return this.localUserRepository.save(localUser);
+    }
+
+    public String loginUser(LoginBody loginBody) {
+        Optional<LocalUser> opUser = localUserRepository.findByUsernameIgnoreCase(loginBody.getUsername());
+
+        if (!opUser.isPresent()) {
+            return null;
+        }
+        LocalUser user = opUser.get();
+        if (!encryptionService.verifyPassword(loginBody.getPassword(), user.getPassword())) {
+            return "";
+        }
+        return jwtService.generateJWT(user);
     }
 }
